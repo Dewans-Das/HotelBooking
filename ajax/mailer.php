f1792956e76599bea40e0f71c73c1432e564e147
Dewans-Das <dewansdas0@gmail.com>
2025-04-05 20:26:37 +0530

    'first

---
+++ ajax/mailer.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'src/PHPMailer.php';
+require 'src/Exception.php';
+require 'src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'ommdas4488@gmail.com'; // Your Gmail address
+    $mail->Password = 'Dewans@2002'; // Your Gmail password or App Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    // Recipients
+    $mail->setFrom('ommdas4488@gmail.com', 'Hotel Booking');
+    $mail->addAddress('dewansdas0@gmail.com', ' DDeewwaannss');
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Email';
+    $mail->Body = 'This is a test email sent using PHPMailer with Gmail SMTP.';
+    $mail->AltBody = 'This is a test email sent using PHPMailer with Gmail SMTP.';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
